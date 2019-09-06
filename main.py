4999bbb96731363850205c762d2ebdc1ee92e214
sarasin <saracsin26@gmail.com>
2019-09-06 17:28:50 -0300

    class Employee @abstract, total_hours method

--- main.py
+++ main.py
@@ -1,55 +1,72 @@
+import abc
 
-
-class Departament:
+class Department:
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
 
-class Employee:
+class Employee(abc.ABC):
+    @abc.abstractmethod
     def __init__(self, code, name, salary):
-        self.__code = code
-        self.__name = name
-        self.__salary = salary
+        self.code = code
+        self.name = name
+        self.salary = salary
+
     def calc_bonus(self):
-        pass
+        # p = Seller(9234, 'Sara', 1500)
+        # print(p.get_sales())
+        return (self.sales * 0.15)
 
     def get_hours(self):
-        pass
+        return 8
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self._departament = Departament('managers', 1)
     
     def calc_bonus(self):
         print(self.salary * 0.15)
         return self.salary * 0.15
     
     @property
-    def departament(self):
-        return self._departament
+    def get_department(self):
+        self.department = Department('managers', 1)
+        return self.department.name
 
-    @departament.setter
-    def set_departament(self, value):
-        self._departament = value
+    @get_department.setter
+    def set_department(self, value):
+        self.department = value
+
 
 class Seller(Manager):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self._departament = Departament('sellers', 2)
         self.sales = 0
 
+    @property
+    def get_sales(self):
+        return self.sales
+
+    @get_sales.setter
+    def put_sales(self, value):
+        self.sales += value
+
     def get_hours(self):
-        return 6
+        return 8
 
     @property
-    def departament(self):
-        return self._departament
+    def get_department(self):
+        self.department = Department('sellers', 2)
+        return self.department.name
 
-    @departament.setter
-    def set_departament(self, value):
-        self._departament = value
+    @get_department.setter
+    def set_department(self, value):
+        self.department = value
 
-x = Departament('Vendas', 828179809234)
+z = Manager(9234, 'Sara', 1500)
+print(z.get_department)
+
+y = Seller(9234, 'Sara', 1500)
+print(y.get_department)
